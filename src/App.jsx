fc128ae75cf7429f3ea314f8081031671c707ce7
Harshitjoshi133 <116452069+Harshitjoshi133@users.noreply.github.com>
2024-06-21 11:26:20 +0530

    Set up the Navbar, Search Button and add more button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import NavigationBar from "./components/NavigationBar";
+import { CiSearch } from "react-icons/ci";
+import { FaPlusCircle } from "react-icons/fa";
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="px-4 max-w-[370px] mx-auto">
+      <NavigationBar></NavigationBar>
+      <div className="flex">
+      <div className="flex flex-grow ml-1 relative items-center">
+        <CiSearch className="absolute text-3xl text-white"/>
+        <input type="text" className=" h-10 flex-grow 
+        bg-transparent border pl-9 text-white border-white rounded-md" />
+      </div>
+     
+        <FaPlusCircle className="text-white text-4xl ml-2 cursor-pointer" />
+
+    
+      </div>
+    </div>
   )
 }
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavigationBar = () => {
+  return (
+    <div className="flex justify-center items-center my-4 gap-2 bg-white h-[60px] text-xl rounded-lg font-medium">
+      
+        <img src="/public/logos_firebase.svg"/>
+        <h1>FireBase Contact App</h1>
+
+    </div>
+  )
+}
+
+export default NavigationBar

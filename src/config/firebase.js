273600e9c9270516c5147aa332883f7f39eb744d
Harshitjoshi133 <116452069+Harshitjoshi133@users.noreply.github.com>
2024-07-09 01:18:27 +0530

    Made the registration Page Ui from the New Guy

---
+++ src/config/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+require('dotenv').config();
+const apiKey = process.env.API_KEY;
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: "vite-contact-79d65.firebaseapp.com",
+  projectId: "vite-contact-79d65",
+  storageBucket: "vite-contact-79d65.appspot.com",
+  messagingSenderId: "601624084811",
+  appId: "1:601624084811:web:1c42a8a6465f5a9cdc68ce"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
